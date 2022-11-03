ec1879a03993d499cf298f799e924487f29cd38f
Piwko <marcinpiwko7@gmail.com>
2022-11-03 22:14:38 +0100

    Add specific permission for user

--- authx/permissions.py
+++ authx/permissions.py
@@ -1,1 +1,21 @@
 from rest_framework.permissions import BasePermission
+
+
+class IsCashierUser(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user.is_authenticated and request.user.role >=0)
+
+
+class IsBaristaUser(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user.is_authenticated and request.user.role >=2)
+
+
+class IsManagerUser(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user.is_authenticated and request.user.role >=3)
+
+
+class IsOwnerUser(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user.is_authenticated and request.user.role >= 4)
