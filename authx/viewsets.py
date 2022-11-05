8161797ceb85f212c0835b9d924ff8c65cbb06ac
Piwko <marcinpiwko7@gmail.com>
2022-11-05 20:00:37 +0100

    Add jwt

--- authx/viewsets.py
+++ authx/viewsets.py
@@ -1,6 +1,9 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, logout
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.response import Response
+from authx.permissions import IsOwnerUser
 from .serializers import UserSerializer
 
 User = get_user_model()
@@ -9,4 +12,12 @@ User = get_user_model()
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()   # .order_by('-date_joined')
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsOwnerUser]
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        logout(request)
+        self.perform_destroy(instance)
+
+        return Response(status=HTTP_204_NO_CONTENT)
